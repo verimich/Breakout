e2fed70cf66f192ab97a567f5599c4f1e50a3d3e
verimich <v.michael2@gmx.net>
2022-06-06 16:23:38 +0200

    push

--- module/icommandabstract.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, ABCMeta, abstractmethod
-
-#Command abtrakte Klasse für das Command Pattern
-class ICommand(metaclass=ABCMeta):
-
-    @abstractmethod
-    def execute():
-        pass
--- module/observerabstract.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, ABCMeta, abstractmethod
-from observersubjectabstract import *
-
-#Basis Klasse für die Observer im Observer Pattern
-class Observer(metaclass=ABCMeta):
-    @abstractmethod
-    def update(self, subject: ObserverSubject):
-        pass

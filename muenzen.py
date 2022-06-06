a9ea9fcac9795af6251a934d59271b82ac2418d3
verimich <v.michael2@gmx.net>
2022-06-06 20:11:25 +0200

    last commit

--- muenzen.py
+++ muenzen.py
@@ -30,24 +30,3 @@ class Muenze:
     def update(self):
         self.muenze_rect.y += self.ys
 
-class FallendesHerz:
-    def __init__(self,x,y):
-        self.x = x 
-        self.y = y 
-        self.ys = 6
-        self.image = ""
-        self.block_rect = ""
-        self.id = 6
-        self.create()
-
-    def create(self):
-        #Sound Herz wird generiert
-        pygame.mixer.Sound.play(soundsettings.muenze_sound)
-        self.image = pygame.image.load(os.path.join(
-        gamesettings.game_folder, 'images/heart.png')).convert_alpha() 
-        self.y += self.image.get_height()/2
-        self.herz_rect = self.image.get_rect(center = (self.x,self.y))
-        
-    def update(self):
-        self.herz_rect.y += self.ys
-
